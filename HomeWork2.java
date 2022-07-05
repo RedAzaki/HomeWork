e3a15c23a86f7fa12f52703bb6a78d1ebb6ce127
”RedAzaki” <azakired@gmail.com>
2022-07-05 23:46:01 +0300

    ddddd

---
+++ HomeWork2.java
@@ -0,0 +1,55 @@
+public class HomeWork2 {
+        public static void main(String[] args) {
+                chekSumm();
+                chekNumber();
+                chekCompare();
+                wordAndNumber();
+
+
+
+
+        }
+
+        public static void chekSumm() {
+                        int a = 3;
+                        int b = 7;
+                        int c = (a + b);
+                        System.out.println(c >= 10 && c <= 20);
+            System.out.println(); // разделяем для комфортного чтения результата.
+                }
+
+
+            public static void chekNumber() {
+                    int a = -5;
+                    if ( a >= 0)
+                            System.out.println("sum is positive");
+                    if ( a < 0)
+                            System.out.println("sum is negative");
+                System.out.println(); // разделяем для комфортного чтения результата.
+
+            }
+
+            public static void chekCompare() {
+                    int a = -1;
+                    System.out.println(a >= 0);
+                System.out.println(); // разделяем для комфортного чтения результата.
+            }
+
+
+        public static void wordAndNumber() {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 1; j++) {
+                    System.out.print("word");
+                }
+                System.out.println();
+            }
+        }
+/* как раотать с аргументами не совсем понятно. что то пытался попробовать сделать, но у меня разногласия идут с main.
+
+ */
+
+            /* делал домашнее задание и только в конце обнаружил подсказки.
+            Там не так как я сделал, но у меня тоже работает согласно условию задачи. Насколько мое решение верное?
+             */
+
+}
